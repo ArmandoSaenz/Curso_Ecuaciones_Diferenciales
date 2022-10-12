dc99d0f4fb61a3bdc39ecd62cc042ae7f0d3cf3f
José Armando Sáenz Esqueda <armando.saenz@diractum.com>
2022-10-12 14:55:55 -0500

    Agregar archivos de proyecto.

---
+++ Curso_Ecuaciones_Diferenciales/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Curso_Ecuaciones_Diferenciales
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbTiempo = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbIncremento = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.tbValorK = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.tbCInicial = new System.Windows.Forms.TextBox();
+            this.btnGraficar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Color = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.k = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P0 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Panel1.Controls.Add(this.btnLimpiar);
+            this.splitContainer1.Panel1.Controls.Add(this.btnGraficar);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox4);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox3);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.plotView1);
+            this.splitContainer1.Size = new System.Drawing.Size(1007, 549);
+            this.splitContainer1.SplitterDistance = 335;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbTiempo);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(195, 50);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tiempo de simulación";
+            // 
+            // tbTiempo
+            // 
+            this.tbTiempo.Location = new System.Drawing.Point(6, 19);
+            this.tbTiempo.Name = "tbTiempo";
+            this.tbTiempo.Size = new System.Drawing.Size(170, 20);
+            this.tbTiempo.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbIncremento);
+            this.groupBox2.Location = new System.Drawing.Point(12, 68);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(195, 50);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Diferencial de tiempo";
+            // 
+            // tbIncremento
+            // 
+            this.tbIncremento.Location = new System.Drawing.Point(6, 19);
+            this.tbIncremento.Name = "tbIncremento";
+            this.tbIncremento.Size = new System.Drawing.Size(170, 20);
+            this.tbIncremento.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tbValorK);
+            this.groupBox3.Location = new System.Drawing.Point(12, 124);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(195, 50);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Valor de K";
+            // 
+            // tbValorK
+            // 
+            this.tbValorK.Location = new System.Drawing.Point(6, 19);
+            this.tbValorK.Name = "tbValorK";
+            this.tbValorK.Size = new System.Drawing.Size(170, 20);
+            this.tbValorK.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.tbCInicial);
+            this.groupBox4.Location = new System.Drawing.Point(12, 190);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(195, 50);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Población inicial";
+            // 
+            // tbCInicial
+            // 
+            this.tbCInicial.Location = new System.Drawing.Point(6, 19);
+            this.tbCInicial.Name = "tbCInicial";
+            this.tbCInicial.Size = new System.Drawing.Size(170, 20);
+            this.tbCInicial.TabIndex = 0;
+            // 
+            // btnGraficar
+            // 
+            this.btnGraficar.Location = new System.Drawing.Point(12, 246);
+            this.btnGraficar.Name = "btnGraficar";
+            this.btnGraficar.Size = new System.Drawing.Size(75, 23);
+            this.btnGraficar.TabIndex = 4;
+            this.btnGraficar.Text = "Graficar";
+            this.btnGraficar.UseVisualStyleBackColor = true;
+            this.btnGraficar.Click += new System.EventHandler(this.btnGraficar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(12, 275);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 5;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // plotView1
+            // 
+            this.plotView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plotView1.Location = new System.Drawing.Point(0, 0);
+            this.plotView1.Name = "plotView1";
+            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.Size = new System.Drawing.Size(668, 549);
+            this.plotView1.TabIndex = 0;
+            this.plotView1.Text = "plotView1";
+            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Color,
+            this.k,
+            this.P0});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 318);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(310, 219);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // Color
+            // 
+            this.Color.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Color.HeaderText = "Color";
+            this.Color.Name = "Color";
+            this.Color.ReadOnly = true;
+            this.Color.Width = 56;
+            // 
+            // k
+            // 
+            this.k.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewCellStyle1.Format = "N2";
+            dataGridViewCellStyle1.NullValue = null;
+            this.k.DefaultCellStyle = dataGridViewCellStyle1;
+            this.k.HeaderText = "Valor de k";
+            this.k.Name = "k";
+            this.k.ReadOnly = true;
+            this.k.Width = 80;
+            // 
+            // P0
+            // 
+            this.P0.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewCellStyle2.Format = "N2";
+            dataGridViewCellStyle2.NullValue = null;
+            this.P0.DefaultCellStyle = dataGridViewCellStyle2;
+            this.P0.HeaderText = "Población inicial";
+            this.P0.Name = "P0";
+            this.P0.ReadOnly = true;
+            this.P0.Width = 99;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 549);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnGraficar;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox tbCInicial;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox tbValorK;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tbIncremento;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbTiempo;
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Color;
+        private System.Windows.Forms.DataGridViewTextBoxColumn k;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P0;
+    }
+}
+
---
+++ Curso_Ecuaciones_Diferenciales/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Legends;
+namespace Curso_Ecuaciones_Diferenciales
+{
+    public partial class Form1 : Form
+    {
+        double P(double P0, double k, double t) => P0 * Math.Exp(-k * t);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGraficar_Click(object sender, EventArgs e)
+        {
+            //Acquire data
+            double k, P0, T, dt;
+            k = double.Parse(tbValorK.Text);
+            P0 = double.Parse(tbCInicial.Text);
+            T = double.Parse(tbTiempo.Text);
+            dt = double.Parse(tbIncremento.Text);
+            //Create Plot
+            PlotModel Grafica = plotView1.ActualModel == null ? new PlotModel() : plotView1.ActualModel;
+            LineSeries Poblacion = new LineSeries();
+            for (double t = 0; t <= T; t += dt)
+                Poblacion.Points.Add(new DataPoint(t, P(P0, k, t)));
+            Grafica.Series.Add(Poblacion);
+            //Add to datagridview
+            dataGridView1.Rows.Add(new object[] { "", k, P0 });
+            MessageBox.Show((Grafica.Series.Last() as LineSeries).Color.R.ToString());
+            plotView1.Model = Grafica;
+            plotView1.InvalidatePlot(true);
+        }
+    }
+}
